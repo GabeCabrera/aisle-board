1261d4727b295aa611edffe29086d8d7205271a0
Gabe Cabrera <gabrielmscabrera@gmail.com>
2025-11-29 18:09:26 -0700

    Disable ESLint during build to fix Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Disable ESLint during build (we'll run it separately)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // Enable experimental features for better performance
   experimental: {
     // Improve cold start times
