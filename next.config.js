a7929d959e8d3892e02b858b03c324103b52afdc
Gabe Cabrera <gabrielmscabrera@gmail.com>
2025-12-02 18:17:11 -0700

    chore: trigger fresh Vercel build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Force fresh build - updated Dec 2 2024
   // Disable ESLint during build (we'll run it separately)
   eslint: {
     ignoreDuringBuilds: true,
